fa0cc50a2582b62516ebde3fd86cc46a037656e2
Juan Cocina <juan_cocina@csu.fullerton.edu>
2022-03-20 13:52:16 -0700

    Abandoning due to oudated information...

--- ReactJS-Mosh-Tutorial/counter-app/src/components/counter.jsx
+++ ReactJS-Mosh-Tutorial/counter-app/src/components/counter.jsx
@@ -1,45 +1,53 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = { //any data that the component needs
-        count: 0,
+        value: this.props.value,
     };
-    /*
-    constructor() { // first we have to call the parent constructor
-        super();
+    
+    constructor(props) { // first we have to call the parent constructor
+        super(props);
         //in javascript, functions are objects, therefore have
         this.handleIncrement = this.handleIncrement.bind(this); //this method returns a new function
     }
-    */
+
     // using an arrow function is faster than the commented out approach above...
     // use arrow to bind handlers...
-    handleIncrement = (id) => { //In this event handler, we do not have access to 'this'
+    handleIncrement = product => { //In this event handler, we do not have access to 'this'
         //inherent a method from base component, we have to tell react what has changed
-        console.log(id);
-        this.setState({ count: this.state.count + 1}) 
+        console.log(product);
+        this.setState({ value: this.state.value + 1}) 
+
 
         //obj.method();
         //function(); by default returns a reference to the window object,
         // if strict mode is enabled, will return undefined...
     }
+    doHandleIncrement = () => {
+        this.handleIncrement({id: 1});
+    };
 
-    render() { 
+    render() {
+
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={ () => this.handleIncrement({id: 1})} 
+                <button onClick={this.doHandleIncrement} 
                 className="btn btn-secondary btn-sm">Incremenet</button>
+
+                <button onClick={() => this.props.onDelete(this.props.id)} 
+                className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "bg m-2 bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state; //we pick the count property from state to use it
+        const { value: count } = this.state; //we pick the count property from state to use it
         return count === 0 ? "Zero" : count; //since jsx can be converted, we can use h1 instead of string
     }
 }
